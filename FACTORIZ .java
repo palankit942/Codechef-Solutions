d609ad8d78f05572fd1d6ffbe8efbb57fe5f9ad5
Ankit kumar pal <palankit942@gmail.com>
2020-12-02 11:30:28 +0530

    Add files via upload

---
+++ FACTORIZ .java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t;
+		t = Integer.parseInt(sc.next());
+		while(t>=1){
+		    
+		    System.out.println("1");
+		    System.out.println(sc.next());
+		    t--;
+		}
+	}
+}
