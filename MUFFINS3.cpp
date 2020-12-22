b38bf781d3be7032cbe30f912bba4876792ae000
Ankit kumar pal <palankit942@gmail.com>
2020-12-22 16:06:17 +0530

    Create MUFFINS3.cpp

---
+++ MUFFINS3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    cout<<(n/2)+1<<endl;
+	}
+	
+	return 0;
+}
