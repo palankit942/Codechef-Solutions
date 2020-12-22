39cef3eb40df1b8f43a20c4a39a295c0d95278cd
Ankit kumar pal <palankit942@gmail.com>
2020-12-22 15:59:11 +0530

    Create FLOW013.cpp

---
+++ FLOW013.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	int a,b,c;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>a>>b>>c;
+	   if(a+b+c==180)
+	   {
+	       cout<<"YES"<<"\n";
+	   }
+	   else
+	   {
+	       cout<<"NO"<<"\n";
+	   }
+	}
+	return 0;
+}
